838922ba296f9f394c902e4f2e2cb9e19b41cec1
ismail <ismailsafha@gmail.com>
2024-01-24 13:45:54 +0100

    fix

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-full h-[100vh]">
+        <div className="w-full h-[100%]">
           {children}
           <Footer />
         </div>
